fb4def76280be91b542cd80cbd1204cfb1e329f8
Maria Eduarda <dudlves17@gmail.com>
2022-01-20 17:14:54 -0300

    criado rota

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const FilmesServices = require('./services/FilmesServices')
+const express = require('express')
 
-console.log(FilmesServices)
+//criando servidor
+const servidor = express()
+
+//criando rotas
+servidor.get('/', (req,res) => {
+    console.log("Alguém fez uma requisição...")
+    res.send("Toma ai o que você me pediu...")
+})
+
+servidor.get('/filme', (req,res) => {
+    console.log("Alguém fez uma requisição...")
+    res.send("filme bom ...")
+})
+
+servidor.get('/generos', (req,res) => {
+    console.log("Alguém fez uma requisição...")
+    res.send("generos")
+})
+
+servidor.get('/filmes/:posicao', (req,res) =>{
+    let posicao = req.params.posicao;
+
+    const filmes = require('./database/filmes.json')
+
+    res.send(filmes[posicao])
+})
+
+
+servidor.listen(3000)
